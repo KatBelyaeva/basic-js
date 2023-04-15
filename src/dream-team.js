360e3759aa7680e913612197afe05e46c7e878c8
KatBelyaeva <kat.belyaeva.it@gmail.com>
2023-04-15 23:18:18 +0300

    Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  firstLetters = [];
+  if (!members) {
+    return false
+  }
+  for (let i = 0; i < members.length; i++) {
+    if (typeof (members[i]) !== 'string') {
+      continue
+    } 
+    else {
+      firstLetters.push(members[i].trim().toUpperCase()[0]);
+    }
+  }
+
+  let sortedLetters = firstLetters.sort();
+  let result = sortedLetters.join('');
+  if (result == '') {
+    return false
+  } else {
+    return result
+  }
 }
 
 module.exports = {
